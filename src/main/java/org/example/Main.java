8f1c6d198d769d63759957b7964fcd546f52a76d
kristijanjakimov <kiko.jakimov@gmail.com>
2024-09-16 13:56:40 +0200

    Task finished

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,26 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+//        * Take a look at the file students.csv. Copy the file to the root directory of your project.
+//        * Use streams to read and output the file line by line (Hint: Files.lines(Path.of("students.csv")))
+//        * Remove the header
+//* Convert each line into the class Student
+//* Remove invalid lines and duplicates
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        List<Integer> myList = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9);
+            List<Integer> myNewList = myList.stream().filter(x -> x % 2 == 0).collect(Collectors.toCollection(ArrayList::new));
+        System.out.println("My new list: " + myNewList);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+            myList.stream().map(x -> x + x)
+                    .sorted()
+                    .forEach(System.out::println);
+            int sum = myList.stream().map(x -> x + x).sorted().reduce(0, Integer::sum);
+        System.out.println("Sum: " + sum);
     }
 }
